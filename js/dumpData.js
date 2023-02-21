f4c32b1b0b97cdbe63e22cd55aa7c1d85a82d3ed
t2011ghtxhieu <t2011gh.txhieu@aptech.vn>
2023-02-21 19:10:34 +0700

    thêm file dumpData

---
+++ js/dumpData.js
@@ -0,0 +1,408 @@
+export const listProducts = [
+	{
+		id: 1,
+		name: 'KRISTALLUM Backlit Bathroom Mirror with Lights',
+		category: ['Trendy', 'Best_Seller','Modern'],
+		types: ['Bathroom'],
+		price: {
+			origin: 900,
+			sale: 800,
+		},
+		shape:'Rectangular',
+		materials: ['Bluetooth','LED'],
+		detailShort: '60x36 Mirror w/ Wireless Switch + Anti Fog/ Waterproof/ Dimmable/ 3 Colors Warm/Natural/White CRI >90 Frameless Led Mirror Vertical or Horizontal',
+		detail: 'Bathroom Vanity Mirror - Introducing the KRISTALLUM High Definition led bathroom mirror featuring a wireless remote switch designed to perfectly fit & light up your space without any hardwire installation. Just Plug and Shine!',
+		img:'./image/Bathroom/71-VCSSCXmL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 2,
+		name: 'SNUGACE Single Beveled Edge Frameless Wall Mount Bathroom Vanity Mirror',
+		category: ['Trendy'],
+		types: ['Bathroom'],
+		price: {
+			origin: 579,
+			sale: 467,
+		},
+		shape:'Oval',
+		materials: ['Metal bordered','Decorative','Glass'],
+		detailShort: '',
+		detail: 'Copper-free silver mirror, environmentally friendly, much less corrosion rate compared with normal silver mirrors used by most brands, hanging both horizontally and vertically',
+		img:'./image/Bathroom/6172sSgnYtL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 3,
+		name: 'ANDY STAR Chrome Mirror for Bathroom',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 790,
+			sale: 700,
+		},
+		shape:'Rectangular',
+		materials: ['Acrylic bordered','Stainless Steel'],
+		detailShort: 'Metal Frame Wall Mirror, Rectangle Stainless Steel Rounded Corner Mirror with 1\'\' Deep Set Design Hangs Horizontal Or Vertical',
+		detail: '2mm thick fingerprint-proof stainless steel frame, upgraded no-distortion 4MM HD mirror to deliver clear reflection. TSCA certified backboard for baby safety. We pay attention to details and take strict quality control from the selection of materials to the manufacturing techniques, just aim to provide a perfect mirror to you',
+		img:'./image/Bathroom/61MG2nxv8BL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 4,
+		name: 'oruii Round Mirror',
+		category: [''],
+		types: ['Living Room','Hallway','Dining Room'],
+		price: {
+			origin: 670,
+			sale: 610,
+		},
+		shape:'Round',
+		materials: ['Aluminum','Metal bordered'],
+		detailShort: 'Round Wall Mirror, Round Bathroom Mirror, Circle Mirrors for Wall, Living Room, Bedroom, Vanity, Entryway, Hallway',
+		detail: 'In terms of the outer frame, brushed aluminum alloy material is used, thus achieving waterproof and durable effect. In the decoration space, it can not only reflect the modernism of home, but also perfectly combine the classical style of home',
+		img:'./image/Bathroom/71O0l5LyD1L._AC_UL480_QL65_.jpg'
+	},
+    {
+		id: 5,
+		name: 'CASSILANDO Arched Mirror',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 970,
+			sale: 890,
+		},
+		shape:'Round',
+		materials: ['Glass','Metal bordered'],
+		detailShort: 'Arched Bathroom Mirror, Metal Frame Mirror, Unique Wall Mounted Mirror, Black Vanity Mirror for Living Room, Fireplace, Bedroom, Entryway ASIN: B0B2VNTZZ9',
+		detail: 'Arched wall mirrors are made of true HD glass that will not distort the image from any Angle. Our fully sealed metal frame & simple design endow the mirror anti-water, shockproof, more stable & longer working life. We use commercial level material to provide a perfect black metal frame mirror to customer',
+		img:'./image/Bathroom/81Jj58PnzZL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 6,
+		name: 'Hasipu LED Bathroom Mirror with Lights',
+		category: [''],
+		types: ['Living Room','Bedroom'],
+		price: {
+			origin: 899,
+			sale: 877,
+		},
+		shape:'Rectangular',
+		materials: ['Aluminum','LED'],
+		detailShort: 'Stepless Dimmable Lighted Wall Mirrors with Anti-Fog, Shatter-Proof, Memory, 3 Colors, Brushed Gold Aluminum Frame Mirror',
+		detail: 'This LED bathroom mirror is multifunctional. Click the button to change the color temperatures: Warm(3000K), Natural (4500K), White(6000K). Long press to dim the bright at comfortable level. Smart memory function will record the lighting setting and keep previous status after turning off. Over 50,000 hours lifespan and energy conservation (Using 3 hours a day means it will last 45 YEARS), especially suitable for hair salon, bathroom, hotel or makeup studio.',
+		img:'./image/Bathroom/71iVM1JrlPL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 7,
+		name: 'MIOZON Backlit Mirror',
+		category: ['Modern'],
+		types: ['Bathroom'],
+		price: {
+			origin: 589,
+			sale: 340,
+		},
+		shape:'Rectangular',
+		materials: ['LED'],
+		detailShort: 'Backlit Mirror for Bathroom Wall with Anti-Fog, Shatter-Proof, Memory Function, LED Vanity Mirror with Lighted, Dimmable Brightness, 3 Colors',
+		detail: 'Using high-quality LED strip light, it is brighter and more durable. Our mirror is bright enough, the light is soft and not dazzling. Bring more refined makeup, shaving and skin care!',
+		img:'./image/Bathroom/71OipC+ul7L._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 8,
+		name: 'Arcus Home Black Bathroom Mirror',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 489,
+			sale: 460,
+		},
+		shape:'Rectangular',
+		materials: ['Metal bordered'],
+		detailShort: 'Black Bathroom Mirror for Wall Vanity Mirror Metal Frame for Modern Farmhouse Home Decor (Horizontal/Vertical)',
+		detail: 'All processes of our mirrors are handmade with more attention to quality and detail. The high quality steel frame takes a very beautiful "seamless design", which is superior to the obvious seams of aluminum frames across the board',
+		img:'./image/Bathroom/61UYJioKdkL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 9,
+		name: 'Catollova Black Bathroom Mirror',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 589,
+			sale: 570,
+		},
+		shape:'Round',
+		materials: ['Metal bordered','Glass'],
+		detailShort: 'Black Wall Mirror, Round Mirror with Black Metal Frame, Also Suitable Circle Mirror for Living Room, Entryway, Dining Room and Bedroom',
+		detail: 'HD mirrors used have more than 90% light reflection ability, ultra-high-definition imaging capability, and are environmentally friendly and lead-free; at the same time, the high-end technology and quality one-piece cast matte metal frame is used, which is also different from other viscose hook-type round mirror',
+		img:'./image/Bathroom/81HhvpIb9HL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 10,
+		name: 'NEUWEABY Black Oval Mirror for Bathroom',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 690,
+			sale: 680,
+		},
+		shape:'Oval',
+		materials: ['Metal bordered','Glass','Aluminum'],
+		detailShort: 'Ovel Wall Mirror Pill Mirrors Wall Mounted Mirror, Large Modern Capsule Mirror Vanity with Black Metal Frame Decor for Entryway, Bedroom, Living Room',
+		detail: 'The modern bathroom mirror with premium aluminum alloy frame. After special anti-corrosion mirror treatment, rust-proof, and fadeless. Thin frame mirror, brushed craft protects its surface from scratches and wearing. Sturdy and robust with high stability.',
+		img:'./image/Bathroom/71cVZdm6nVL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 11,
+		name: 'Growsun Black Round Mirror',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 960,
+			sale: 700,
+		},
+		shape:'Round',
+		materials: ['Acrylic bordered'],
+		detailShort: 'Circle Wall Mirror for Bathroom Vanity, Entryway Living Room with Metal Frame for Home Decor',
+		detail: '4/5 inch width metal frame, unique FORWARD-FLIP design for cover the gap between the frame and glass, more beautiful than others. Matte black/gold/retro gold three colors available.',
+		img:'./image/Bathroom/71UZFQv23mL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 12,
+		name: 'SUPER DEAL Black Wall Mirror for Bathroom',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 300,
+			sale: 200,
+		},
+		shape:'Rectangular',
+		materials: ['Rubber', 'Glass', 'Stainless Steel', 'Wooden framed'],
+		detailShort: 'Wall Mounted Rectangular Entryways Decor Vanity Mirrors with Stainless Steel Metal Frame and Rounded Edge',
+		detail: ' The Rectangular mirror is composed of high-quality metal frame and 22x30 inch silver mirror. The MDF board is added to the back of the mirror by special technology. Even if the mirror is accidentally broken, the broken lens will not splash, but stick to the MDF board, which is very safe.',
+		img:'./image/Bathroom/61L4uzwRVEL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 13,
+		name: 'Koonmi Black Bathrooms Mirror for Wall',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 710,
+			sale: 590,
+		},
+		shape:'Rectangular',
+		materials: ['Glass','Aluminum','Wooden framed'],
+		detailShort: 'Vanity Mirror with Rounded Corner, Rectangular Glass Aluminum Alloy Frame for Bedrooms, Entryways, and Living Rooms Hangs Horizontal or Vertical',
+		detail: 'Modern textured brushed aluminum alloy frame, fine workmanship, waterproof protective surface makes the mirror more corrosion-resistant, an explosion-proof film is attached to the back of the mirror, which can effectively prevent the glass from splashing after the mirror is damaged. Protect your beloved family and friends from harm',
+		img:'./image/Bathroom/71HmAAT6jwL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 14,
+		name: 'Ruomeng Rectangular Wall Mirror',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 570,
+			sale: 437,
+		},
+		shape:'Rectangular',
+		materials: ['Glass', 'Polystyrene','Acrylic bordered'],
+		detailShort: 'Wall Mirror for Bathroom, Bedroom, Entryway, Living Room, White',
+		detail: '1.5cm thick and 4cm wide frame+3mm no-distortion HD float silver glass to deliver clear reflection+high quality explosion-proof membrane for safety. We use commercial level material to provide a perfect wall mounted mirror to customer',
+		img:'./image/Bathroom/61owBMI4SQL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 15,
+		name: 'Black Wall Framed Rectangular Mirrors for Bathrooms',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 827,
+			sale: 600,
+		},
+		shape:'Rectangular',
+		materials: ['Iron','Metal bordered'],
+		detailShort: 'Large Rectangle Mirror with Brushed Glass Panel, Modern Home Entryway Decor Mirror with Corner Deep Design, Hangs Horizontal or Vertical',
+		detail: 'The large 24inch x 36inch black bathroom mirror with a contemporary round mirror corner and black metal frame matches any bathroom, bedroom, washroom or entryway',
+		img:'./image/Bathroom/71RfpvbqroL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 16,
+		name: 'Manocorro Black Oval Wall Mirror',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 628,
+			sale: 590,
+		},
+		shape:'Oval',
+		materials: ['Glass','Aluminum','Acrylic bordered'],
+		detailShort: 'Large Vanity Decor Mirror w/ Metal Frame, Vertical & Horizontal, Prefect for Entryway, Bedroom, Fireplace, Washroom',
+		detail: 'The pill shape design of this wall mounted mirror is perfect for any room and any deco with an aluminum alloy frame to avoid rust and corrosion, it lasts longer and it is very safe to hang in the bathroom',
+		img:'./image/Bathroom/71HA08beqFL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 17,
+		name: 'CASSILANDO Irregular Wall Mirror',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 945,
+			sale: 950,
+		},
+		shape:'Oval',
+		materials: ['Glass','Wooden framed'],
+		detailShort: 'Asymmetrical Mirror Wall Mounted, Unique Vanity Mirror,Shaped Dressing Mirror Decorative for Living Room, Bathroom, Bedroom, Entryway Wall Decor',
+		detail: 'High quality float glass is used to make mirrors to ensure More clear and realistic images. Wood frame, elegant black. simpler and more textured, durable and colorfast. No odor, safe and environmentally friendly',
+		img:'./image/Bathroom/61wxeJLP3WL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 18,
+		name: 'OKPAL 72X32 INCH RGB LED Bathroom Mirror',
+		category: [''],
+		types: ['Bathroom'],
+		price: {
+			origin: 990,
+			sale: 980,
+		},
+		shape:'Rectangular',
+		materials: ['LED','Bluetooth'],
+		detailShort: 'Bathroom Mirror with Backlit Light, Anti-Fog, Dimmable, Multicolor Light, Memory Function, Smart Touch, Shatterproof Glass, Bathroom Vanity Mirror Mounted for Wall',
+		detail: 'An ideal RGB mirror for bathroom, very simple classic, muti-size and muti-color perfectly go with any bathroom, suitable for contemporary, modern, traditional and farmhouse style',
+		img:'./image/Bathroom/51XtueJa5aL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 19,
+		name: 'XIHACTY 2 Piece Wall Mirrors',
+		category: ['Diamond'],
+		types: ['Living Room'],
+		price: {
+			origin: 595,
+			sale: 546,
+		},
+		shape:'Round',
+		materials: ['Decorative','Acrylic bordered'],
+		detailShort: 'Crush Diamond Mirrors Wall Decor, Silver Decorative Mirrors for Living Room Decor, Bedroom, Bathroom',
+		detail: 'This is a mirror with crystal decor, a circle of crush diamonds, The shining mirror covered the diamonds and is very easy to clean. It\'s really cute and gorgeous.This silver wall decor can hangs horizontally or vertically，can also hang the mirror on the wall with chains. Hanging hardware and chains are included. It\'s pretty sturdy and it looks great.',
+		img:'./image/Living/81jpH1DINLL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 20,
+		name: 'MIRUO Full Length Mirror Decor Wall Mounted',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 798,
+			sale: 690,
+		},
+		shape:'Rectangular',
+		materials: ['Decorative','Aluminum'],
+		detailShort: 'Floor Dressing Make Up Mirror Bathroom/Bedroom/Living Room/Dining Room/Entry, Gold',
+		detail: 'It can be freely leaned on the wall directly, or on the floor by standing holder, you just need take out the holder. There are 2 holes in the back of the mirror, just nail two complimentary screws on the wall and hang the mirror. Please find the installation method in the instruction manual.',
+		img:'./image/Living/610ZL9VDKCL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 21,
+		name: 'Kemmandi 10 Pieces Hexagonal Mirror Wall Mirror',
+		category: [''],
+		types: ['Living Room','Bedroom'],
+		price: {
+			origin: 846,
+			sale: 800,
+		},
+		shape:'Hexagonal',
+		materials: ['Metal bordered','Glass'],
+		detailShort: 'Wall Mirror Glass Mirror Mirrors Decor for Home Bedroom Living Room',
+		detail: 'Mirror tile self-adhesive is made of glass, the classic hexagonal design gives you a clear picture. After using the mirror wall sticker, your house will look very spacious and bright. It is a good choice for home decoration.',
+		img:'./image/Living/61VbErUXaUL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 22,
+		name: 'HA Black&Gold Framed Rectangle Bathroom Mirrors for Wall',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 245,
+			sale: 145,
+		},
+		shape:'Rectangular',
+		materials: ['Wooden framed','Decorative'],
+		detailShort: 'Wall Mounted Vintage Mirror Vertical or Horizontal Hanging for Living Room,Bedroom',
+		detail: 'The black&gold framed wall mounted mirror is perfectly designed for vintage or classical design, suit for your bedroom, bathroom living room.Wall mounted mirror can be hung horizontally or vertically, all mounting hardware are included.',
+		img:'./image/Living/71PxI3wmIxL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 23,
+		name: 'Vaabee Family Tree Wall Decor Acrylic 3D DIY Mirror',
+		category: ['Modern'],
+		types: ['Living Room','Bedroom', 'Dinning Room'],
+		price: {
+			origin: 642,
+			sale: 589,
+		},
+		shape:'Rectangular',
+		materials: ['Plastic','Decorative'],
+		detailShort: 'Family Tree Wall Decor Acrylic 3D DIY Mirror Stickers Picture Frame Collage Home Decorations for Living Room Bedroom Dinning Office New House Gifts Silver Set Large 47x47 Inch',
+		detail: 'Made of Acrylic and Self Adhensive, Come with 36 pcs Pieces Stickers, Need to DIY assemble them by the sentcil paper, Peel and Stick on the wall like the puzzle, the end will create an eye-catching wall art for your room.Bright and reflective surface with 2mm thickness, makes your home looks spacious, attractive and brighter.Perfect house decor for living room, farmhouse , kitchen, office, teen room, dress room, dym. etc.',
+		img:'./image/Living/61LJM6qRDQS._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 24,
+		name: 'Chende Mirror for Wall Decor Living Room',
+		category: [''],
+		types: ['Living Room'],
+		price: {
+			origin: 856,
+			sale: 800,
+		},
+		shape:'Rectangular',
+		materials: ['Wooden framed','Decorative'],
+		detailShort: 'Farmhouse Decorative Mirror for Entryway, Large Bathroom Mirror with Black Wood Frame, Accent Mirror',
+		detail: 'This decorative mirror was designed with 2 colors splicing that makes the mirror more special. It is difficult to see in the marke, so make your home decoration different from others.The 4.5 inches frame have a clear texture looking. Even from the side, the border will have a little bump effect to make the texture more beautiful.',
+		img:'./image/Living/61w5GkFNIvL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 25,
+		name: 'Honiway Wall Mirror',
+		category: [''],
+		types: ['Living Room','Bedroom'],
+		price: {
+			origin: 757,
+			sale: 734,
+		},
+		shape:'Sunburst',
+		materials: ['Wooden framed','Decorative'],
+		detailShort: 'Decorative 12 inch Rustic Wood Mirror Sunburst Boho Mirror for Entryway Bedroom Living Room Carbonized Black',
+		detail: 'Rustic Wall Decor for Living Room—This decorative mirror made of natural solid wood, torched finished features carbonized black color, and the beautiful wood texture makes it an antique accent. This stylish and eye-catching decor piece is the perfect addition to your house.',
+		img:'./image/Living/81rIa5UgmvL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 26,
+		name: 'Framed Bevel Leaner Full Length Huge Floor Mirror',
+		category: ['Modern'],
+		types: ['Living Room','Bedroom'],
+		price: {
+			origin: 467,
+			sale: 450,
+		},
+		shape:'Rectangular',
+		materials: ['Wooden framed'],
+		detailShort: 'Full Length Huge Floor Mirror XL Mirror Large Rectangle Standing Floor Mirror Huge Mirrors for Bedroom Oversize Mirrors for Floor XL Floor Mirror - Oil Rubbed Bronze, 66" L x 32" W',
+		detail: 'Our Standing Mirror is simple and modern in design, which gives it a sophisticated look that makes it a perfect accent piece for any room. This Large Mirror is enough for you to see your full outfit in a single glance. The Floor & Full Length Mirrors can even help you check how your shoes match your entire outfit.Our Long Mirror is crafted with a durable Faux Wooden Frame. This Big Mirror not only looks elegant, but it is sturdy, rust-resistant, and built to last forever. Daily care of this Floor Length Mirror is also very hassle-free, requiring only a little wiping down.',
+		img:'./image/Living/71AZvh+PDGL._AC_UL480_QL65_.jpg'
+	},
+	{
+		id: 27,
+		name: 'LFT HUIMEI2Y Full Length Floor Mirror',
+		category: [''],
+		types: ['Living Room','Bedroom'],
+		price: {
+			origin: 789,
+			sale: 755,
+		},
+		shape:'Rectangular',
+		materials: ['Glass', 'Aluminum','Acrylic bordered'],
+		detailShort: 'Full Length Floor Mirror with Standing Holder, 63"x18" Aluminum Body Mirror, Rectangle Mirror, Hanging Mirror, Standing Large Wall Mounted Mirror for Bedroom or Living Room, Silver',
+		detail: 'The 63" x18" large dressing mirror can let you see yourself with one glance, full length mirror will suit your needs! These floor mirrors have three colors for you to choose from: gold, black, silver, you can put them on the living room, bedroom, dressing room, closets, foyer, corridor, etc.',
+		img:'./image/Living/61nIq4kOjLL._AC_UL480_QL65_.jpg'
+	},
+];
+
